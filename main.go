186a50945b06b56ff4d5cac962c17e31387074eb
hayajo_77 <hayajo_77@hatena.ne.jp>
2022-06-27 10:46:26 +0900

    create webhook

--- main.go
+++ main.go
@@ -74,6 +74,10 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&corev1.Pod{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Pod")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
